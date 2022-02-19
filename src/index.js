721c96c10decc47c230ac790bf527215d87e9a53
Max Kovalenko <tutmrmaxi@gmail.com>
2022-02-19 21:13:06 +0300

    feat: add ugly solving

--- src/index.js
+++ src/index.js
@@ -38,7 +38,13 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let re=/(.{10}|.)/g
+    let re2 =/(.{2}|.)/g;
+    let arr = expr.match(re);
+    let arr2 = arr.map(e => e.match(re2));
+    let arr3 = arr2.map(el => el.filter(el => el!=='00'));
+    let arr4 = arr3.map(el => el.map(el => el == '10' ? '.' : el == '11'? '-' : ' ').join(''));
+    return(arr4.map(elem => elem = MORSE_TABLE[elem] || ' ').join(''));
 }
 
 module.exports = {
